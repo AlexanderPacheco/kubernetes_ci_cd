c9e4e7895c83febb2310adf3b3335d16e63d7af7
Alexander <alexixva@gmail.com>
2022-02-20 17:59:55 -0600

    Update credentials

--- BACKEND/app/config/s3.env.js
+++ BACKEND/app/config/s3.env.js
@@ -1,6 +1,6 @@
 const env = {
-	AWS_ACCESS_KEY: 'AKIAWXVVL4U54PYGDUV7', // change to yours
-	AWS_SECRET_ACCESS_KEY: 'TyGWSiElJa49sgDBwMgWFvJ5O3low4JGUd61HM2L', // change to yours
+	AWS_ACCESS_KEY: 'AWS_ACCESS_KEY', // change to yours
+	AWS_SECRET_ACCESS_KEY: 'AWS_SECRET_ACCESS_KEY', // change to yours
 	REGION : 'us-east-2', // change to yours
 	Bucket: 'ayd2-files' // change to yours
 };
